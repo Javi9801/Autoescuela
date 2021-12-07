c8b44e50bcca213dc259c6168e44a78e998efe83
Javier Jimenez Diaz <jjimdia0908@g.educaand.es>
2021-12-07 14:21:34 +0100

    07/12/2021

--- js/funcionesAdicionales.js
+++ js/funcionesAdicionales.js
@@ -1,17 +1,7 @@
 window.addEventListener("load", function(){
     const imagen = document.getElementById("imagen_perfil");
     const div = document.getElementById("informacion_usuario");
-    const preguntas = document.getElementById("preguntas_examen");
-    const siguiente = document.getElementById("siguiente");
-    const anterior = document.getElementById("anterior");
 
-    this.document.getElementById("pre_examen0").className="selec";
-
-    for(let i=1; i<preguntas.children.length;i++){
-        var aux = i+1+"";
-        var p = "pre_examen"+i;
-        document.getElementById(p).className= "escondido";
-    }
 
     imagen.onclick = function(){
         if(div.style.visibility == "hidden"){
@@ -21,35 +11,6 @@ window.addEventListener("load", function(){
 
         }
 
-    }
-
-    siguiente.onclick = function(){
-        for(let i=0; i<preguntas.children.length;i++){
-
-            if(i!=preguntas.children.length-1){
-                if(preguntas.children[i].className == "selec"){
-                    preguntas.children[i].classList.remove("selec");
-                    preguntas.children[i].className="escondido";
-                    preguntas.children[i+1].classList.remove("escondido");
-                    preguntas.children[i+1].className="selec";
-                    break;
-                }
-            }
-        }
-    }
-
-    anterior.onclick = function(){
-        for(let i=0; i<preguntas.children.length;i++){
-            if(preguntas.children[i].className == "selec"){
-                if(i!=0){
-                    preguntas.children[i].classList.remove("selec");
-                    preguntas.children[i].className="escondido";
-                    preguntas.children[i-1].classList.remove("escondido");
-                    preguntas.children[i-1].className="selec";
-                    break;
-                }
-            }
-        }
     }
 
 })
--- realizarExamen.php
+++ realizarExamen.php
@@ -6,10 +6,11 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
     <script src="js/funcionesAdicionales.js"></script>
+    <script src="js/funcionesExamen.js"></script>
 </head>
 <body>
-    <?php 
-    
+    <?php
+
     require_once("cargadores/cargarHelper.php");
     require_once("cargadores/cargarEntidades.php");
     include ("includes/nav.php");
@@ -21,62 +22,85 @@
 
     $p = BD::obtienePreguntasExamen($examen->id);
     $n_preg = count($p);
-    
+
     ?>
 
-    <section class="contenido">     
+    <section class="contenido">
 
         <?php
-        
+
         sesion::iniciar();
         $u = sesion::leer('usuario');
         $rol = BD::obtieneRol($u->rol);
 
         if($rol->id==1){
         ?>
-
-    
-            <h1 class="h1_preguntas"><?php echo $examen->descripcion?></h1>
-
-            <section id="preguntas_examen">
-                <?php
-
-                for($i=0; $i<$n_preg; $i++){
-                    $r = BD::obtieneRespuestas($p[$i]->id);
-                    $n_resp = count($r);
-                ?>
-
-                <section id="pre_examen<?php echo $i ?>">
-                    Enunciado <br><textarea name="enun" id="enun" cols="30" rows="10"><?php echo $p[$i]->enunciado?></textarea>
-
+            <form action="" method="POST">
+                <h1 class="h1_preguntas"><?php echo $examen->descripcion?></h1>
+                <section id="preguntas_examen">
                     <?php
 
-                        for($j=1; $j<$n_resp; $j++){
-                        ?>
-                        
-                        <p>
-                            <label for="pregunta_respuesta_examen_<?php echo $j ?>">Opcion <?php echo $j ?></label>
-                            <input type="text" id="pregunta_respuesta_examen_<?php echo $j ?>" name="pregunta_respuesta_examen_<?php echo $j ?>" value="<?php echo $r[$j]->enunciado ?>">
-                            <input type="radio" id="opcion_examen_<?php echo $j ?>" value="opcion<?php echo $j ?>" name="opciones<?php echo $i ?>"> Correcta
-                        </p>
+                    for($i=0; $i<$n_preg; $i++){
+                        $r = BD::obtieneRespuestas($p[$i]->id);
+                        $n_resp = count($r);
+                    ?>
+
+                    <section id="pre_examen<?php echo $i ?>">
+
+                        <h2>Pregunta <?php echo $i+1 ?></h2><br>
+                        <br><textarea disabled name="enun" id="enun" cols="45" rows="5"><?php echo $p[$i]->enunciado?></textarea>
+
                         <?php
-                        }
-                        ?>
 
-                </section>
+                            for($j=1; $j<$n_resp; $j++){
+                            ?>
 
-           
+                            <p>
+                                <label for="pregunta_respuesta_examen_<?php echo $j ?>">Opcion <?php echo $j ?></label>
+                                <input type="text" id="pregunta_respuesta_examen_<?php echo $j ?>" disabled name="pregunta_respuesta_examen_<?php echo $j ?>" value="<?php echo $r[$j]->enunciado ?>">
+                                <input type="radio" id="opcion_examen_<?php echo $j ?>" value="opcion<?php echo $j ?>" name="opciones<?php echo $i ?>"> Correcta
+                            </p>
+                            <?php
+                            }
+                            ?>
 
-            <?php
+                    </section>
+
+
+
+                <?php
+                }
+
             }
-
-        }
-        ?>
-        </section>
-        <input type="button" id="anterior" class="anterior" value="Anterior">
-         <input type="button" id="siguiente" class="siguiente" value="Siguiente">
+            ?>
+                </section>
+                <input type="submit" class="btn_enviar" id="finalizar_examen" name="finalizar_examen" value="Finalizar">
+            </form>
+            <section class="botones_examen">
+                <input type="button" id="anterior" class="anterior" value="Anterior">
+                <input type="button" id="siguiente" class="siguiente" value="Siguiente">
+            </section>
     </section>
 
     <?php include ("includes/footer.php");?>
 </body>
 </html>
+
+
+<?php
+
+    if(isset($_POST["finalizar_examen"])){
+        for($i=0; $i<$n_preg; $i++){
+            for($j=1; $j<$n_resp; $j++){
+                if($_POST['opciones'.$i.''] == 'opcion'.$j.''){
+                    $a = $p[$i]->id;
+                    $b = $r[$j]->id;
+                }
+            }
+        }
+
+
+
+    }
+
+?>
